1e17f5b3cfa1a7934a2da3ad9f1da08b75d39825
Gabriel <darkmash@web.de>
2020-11-13 12:10:42 +0100

    make setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cHc',
+    version='0.1',
+    packages=['chc'],
+    url='https://github.com/gabelstein/cHc',
+    license='',
+    author='Gabriel',
+    author_email='gabriel@bccn-berlin.de',
+    description=''
+)
